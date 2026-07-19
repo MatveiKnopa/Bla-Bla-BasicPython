d2ef915e888649bff873e37f36a7229e6cb2945f
MatveiKnopa <popysk629@gmail.com>
2026-07-19 13:30:48 +1000

    random

---
+++ 17/main.py
@@ -0,0 +1,16 @@
+a = int(input())
+b = input()
+c = ""
+lst = list(b)
+for i in range(0,10,1):
+    lst.append(str( i))
+import random
+random.shuffle(lst)
+for j in range(10):
+    c = ""
+    for i in range(a):
+        num = random.randint(0,len(lst)-1)
+        c = c + lst[num]
+    print(c)
+ 
+6
